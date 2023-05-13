c70261aa04406d785cf06cf4b3679b0b32e2bf57
miapristinskaya <mia.pristinskaya@gmail.com>
2023-05-13 15:05:02 +0300

    callback form is opened with click on every button, took out common classes for buttons and headings, lowered overlay html

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-let button = document.querySelector(".booking_button");
+let buttonsCall = document.querySelectorAll(".button_call");
 let overlay = document.querySelector(".overlay");
 let callback = document.querySelector(".callback");
 
-button.addEventListener("click", function () {
-  overlay.style.display = "flex";
+buttonsCall.forEach(function (button_call) {
+  button_call.addEventListener("click", function () {
+    overlay.style.display = "flex";
+  });
 });
 
 overlay.addEventListener("click", function () {
@@ -11,5 +13,5 @@ overlay.addEventListener("click", function () {
 });
 
 callback.addEventListener("click", function (e) {
-  e.stopProapagation();
+  e.stopPropagation();
 });
